5c3f3760ce003c3d5ffc9d9523ad44aea8f3081f
N2TS-Dolphin <117240825+N2TS-Dolphin@users.noreply.github.com>
2023-10-29 18:05:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 function buttonClick() {
-    let x = document.getElementById("num1").value;
-    let y = document.getElementById("num2").value;
+    let x = document.getElementById("fnum1").value;
+    let y = document.getElementById("fnum2").value;
 
-    if (x === "" || y === "") {
-      alert("Vui lòng nhập cả hai số trước khi tính toán.");
+    if (x === "" && y === "") {
+      alert("Vui lòng nhập cả 2 số trước khi tính toán.");
+      return; // Dừng hàm nếu có một hoặc cả hai ô không được nhập giá trị
+    }
+    else if (x === "") {
+      alert("Vui lòng nhập số thứ 1 trước khi tính toán.");
+      return; // Dừng hàm nếu có một hoặc cả hai ô không được nhập giá trị
+    }
+    else if (y === "") {
+      alert("Vui lòng nhập số thứ 2 trước khi tính toán.");
       return; // Dừng hàm nếu có một hoặc cả hai ô không được nhập giá trị
     }
 
